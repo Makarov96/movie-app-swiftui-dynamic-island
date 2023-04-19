0296e14870408253ed3dc0b1ec1f76fd6820ca16
Makarov96 <estev97@gmail.com>
2023-04-19 14:10:26 -0600

    Initial Commit

---
+++ movies/moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesApp.swift
+//  movies
+//
+//  Created by Guerin Steven Colocho Chacon on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
